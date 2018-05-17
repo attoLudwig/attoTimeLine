9f2fcac781a728a8853fd46368f7b650d842e45b
attoLudwig <36920414+attoLudwig@users.noreply.github.com>
2018-05-17 13:08:49 +0200

    Update read_in.py

--- read_in.py
+++ read_in.py
@@ -1,8 +1,11 @@
 import pandas as pd
-from Timeline import db
+from sqlalchemy import create_engine
 import timeit
 import tracking_f as tr
 import priority_calc as prio
+import manipulate_df as m_df
+import check_qs as c_qs
+import config
 import numpy as np
 
 
@@ -11,7 +14,8 @@ start = timeit.default_timer()
 """
     Read authentification details from auth.txt
 """
-with open("../Data/auth.txt") as f:
+Auth_File = config.auth_file()
+with open(Auth_File) as f:
     auth = f.readlines()
 
 auth = [x.strip() for x in auth]
@@ -35,30 +39,48 @@ auth = list(filter(None, auth))
 
 """
 
+"""
+    Run SQL query via pandas
+"""
+sql_string = config.sql_str_return()
+engine = create_engine('mssql+pyodbc://@python')
+con = engine.connect()
+df = pd.read_sql(sql_string, con)
 
-"""
-    clear database
-    CHECK: Performance impact too large? Maybe better to update DB?
-           -> More coding effort
-"""
-UHDs = UHD.query.all()
-for u in UHDs:
-            db.session.delete(u)
-db.session.commit()
 
-"""
-    load excel sheet into dataframe
-    Future: Run SQL query via pandas
-"""
-df = pd.read_excel('../Data/Timeline_v27.xlsm', 'Data',
-            skiprows=range(0,2),
-            usecols=range(0,23),
-            dtype={'UHD':str,'RMA':str})
+# Data_File = '/Users/Ludwig/Documents/Arbeit/Python/Data/Timeline_v27.xlsm'
+#
+# df = pd.read_excel(Data_File, 'Data',
+#             skiprows=range(0,2),
+#             usecols=range(0,23),
+#             dtype={'UHD':str,'RMA':str})
+
+
+# '''
+#     Drop ShippingStatus to fix Type error
+# '''
+# df.drop(['ShippingStatus'], axis=1)
 
 """
     remove duplicates
 """
 df.drop_duplicates(subset='UHD', inplace=True)
+
+
+"""
+    Check QS
+"""
+QS_POS = config.QS_POS_path()
+df_QS_POS = pd.read_excel(QS_POS, "Quelle")
+
+QS_SEN = config.QS_SEN_path()
+df_QS_SEN = pd.read_excel(QS_SEN, "Vortest")
+
+
+'''
+    Add Columns
+'''
+df = m_df.add_columns(df)
 
 """
     initialise Priority Dataframe from excel file
@@ -70,53 +92,51 @@ PrioDF = prio.initPrio()
 """
 for i in range(0,len(df.UHD)):
 
+            print("\n --- Row: ", i, " ---")
+
+            if df.POS.iloc[i] == "POS":
+                #print("POS")
+                df.QS1.iloc[i], df.QS2.iloc[i] = c_qs.QS_POS(df.SN.iloc[i], df_QS_POS)
+            elif df.POS.iloc[i] == "SEN" and df.SN.iloc[i] != None:
+                #print("SEN")
+                df.QS1.iloc[i], df.QS2.iloc[i] = c_qs.QS_SEN(df.SN.iloc[i], df_QS_SEN)
+            else:
+                print("MIC")
+                #df.QS1.iloc[i], df.QS2.iloc[i] = pd.NaT, pd.NaT
+
+
             """  find status  """
-            Status = prio.status_calc(df.loc[i,:])
+            #print("Status")
+            df.Status.iloc[i] = int(prio.status_calc(df.iloc[i]))
 
             """  find LastStep and ShippingStatus """
-            LastStep, ShippingStatus = calc_LastStep(df.loc[i,:], Status)
+            #print("LastStep")
+            df.LastStep.iloc[i], df.ShippingStatus.iloc[i] = prio.calc_LastStep(df.iloc[i], df.Status.iloc[i], auth)
 
             """ get priority type from dataframe, e.g. POS2 """
-            PriorityType = df.POS[i] + str(df.Priority[i])
+            #print("Prio")
+            if int(df.Priority.iloc[i]) != 6 and int(df.Priority.iloc[i]) != '':
+                df.PriorityType.iloc[i] = df.POS.iloc[i] + str(int(df.Priority.iloc[i]))
+            else:
+                df.PriorityType.iloc[i] = df.POS.iloc[i] + str(int(0))
+            #print "POS/SEN: ", df.POS[i], " --- Priority: ", df.Priority[i]
 
-            """ find Waiting time and check if step is overdue """
-            waiting, overdue = prio.calc_waiting(PrioDf, PriorityType, Status, LastStep)
 
-            """ write to database from dataframe """
-            data = UHD(
-                        DatumBeginn=df.DatumBeginn[i],
-                        UHD=df.UHD[i],
-                        POSSEN=df.POS[i],
-                        Titel=df.Titel[i],
-                        Bearbeiter=df.Bearbeiter[i],
-                        Notizen=df.Notizen[i],
-                        Customer=df.Customer[i],
-                        CustNo=df.CustNo[i],
-                        Priority=df.Priority[i],
-                        SN=df.SN[i],
-                        RMA=df.RMA[i],
-                        RepAngebot=df.RepAngebot[i],
-                        RepAuftr=df.RepAuftr[i],
-                        DatumRetAngebot=df.DatumRetAngebot[i],
-                        DatumAngekommen=df.DatumAngekommen[i],
-                        DatumRepAngebot=df.DatumRepAngebot[i],
-                        DatumRepAuftr=df.DatumRepAuftrag[i],
-                        QS1=df.QS1[i],
-                        QS2=df.QS2[i],
-                        DateShipped=df.DateShipped[i],
-                        Wiedervorlage=df.Wiedervorlage[i],
-                        TrackingID=df.TrackingID[i],
-                        ShippingStatus=df.ShippingStatus[i],
-                        Status=Status,
-                        Waiting=waiting,
-                        LastStep=LastStep,
-                        Overdue=overdue
-                        )
-
-            db.session.add(data)
-            db.session.commit()
-
+            if not df.POS.iloc[i] == " ":
+                #print("Waiting")
+                """ find Waiting time and check if step is overdue """
+                df.Waiting.iloc[i], df.Overdue.iloc[i] = prio.calc_waiting(PrioDF, df.PriorityType.iloc[i], df.Status.iloc[i], df.LastStep.iloc[i])
 
 stop = timeit.default_timer()
 
 print("Runtime: ", stop-start, "s")
+
+#df = m_df.change_time(df)
+
+start = timeit.default_timer()
+
+df.to_csv('Database.csv', encoding='utf-8', date_format='%d.%m.%Y')
+
+stop = timeit.default_timer()
+
+print("Time to write csv: ", stop-start, "s")

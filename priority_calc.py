6b89920309bd406fa4cd97a1fd1dd9b9d61ec433
attoLudwig <36920414+attoLudwig@users.noreply.github.com>
2018-05-17 13:09:19 +0200

    Update priority_calc.py

--- priority_calc.py
+++ priority_calc.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import tracking_f as tr
+import config
 from datetime import datetime
+import numpy as np
 
 def days_between(d1, d2):
-    d1 = datetime.strptime(d1, "%Y-%m-%d")
-    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    #d1 = datetime.strptime(d1, "%Y-%m-%d")
+    #d2 = datetime.strptime(d2, "%Y-%m-%d")
     return abs((d2 - d1).days)
 
 """
@@ -12,30 +14,33 @@ def days_between(d1, d2):
     Stored in Excel file for easy editing!
 """
 def initPrio():
-            PrioDf = pd.read_excel('../Data/Prio.xlsx', 'Priorities',
+            Prio_Data = config.prio_excel_path()
+            PrioDf = pd.read_excel(Prio_Data, 'Priorities',
                         userows=range(0,20),
                         usecols=range(0,10),
                         )
             return PrioDf
 
 """
-    get status of UHD 
+    get status of UHD
     To do: Check if None needs to be replaced by NaT
 """
 def status_calc(df):
-            if df.DateShipped != None:
+            #print pd.isnull(df.DateShipped)
+
+            if not pd.isnull(df.DateShipped):
                         Status = 8
-            elif df.QS2 != None:
+            elif not pd.isnull(df.QS2):
                         Status = 7
-            elif df.QS1 != None:
+            elif not pd.isnull(df.QS1):
                         Status = 6
-            elif df.RepAuftr != None:
+            elif not pd.isnull(df.DatumRepAuftrag):
                         Status = 5
-            elif df.RepAngebot != None:
+            elif not pd.isnull(df.DatumRepAngebot):
                         Status = 4
-            elif df.DatumAngekommen != None:
+            elif not pd.isnull(df.DatumAngekommen):
                         Status = 3
-            elif df.DatumRepAngebot != None:
+            elif not pd.isnull(df.DatumRetAngebot):
                         Status = 2
             else:
                         Status = 1
@@ -43,7 +48,7 @@ def status_calc(df):
             return Status
 
 """ get date of last completed step """
-def calc_LastStep(df, Status):
+def calc_LastStep(df, Status, auth):
 
             ShippingStatus = ''
 
@@ -54,9 +59,9 @@ def calc_LastStep(df, Status):
             elif Status == 3:
                         LastStep = df.DatumAngekommen
             elif Status == 4:
-                        LastStep = df.RepAngebot
+                        LastStep = df.DatumRepAngebot
             elif Status == 5:
-                        LastStep = df.RepAuftr
+                        LastStep = df.DatumRepAuftrag
             elif Status == 6:
                         LastStep = df.QS1
             elif Status == 7:
@@ -65,10 +70,17 @@ def calc_LastStep(df, Status):
                         """ Status = 8 -> shipped.
                             find tracking status
                         """
-                        if get_carrier(df.TrackingID[i]) == 'UPS':
-                                    ShippingStatus, LastStep = tr.ups_track(df.TrackingID[i])
-                        if get_carrier(df.TrackingID[i]) == 'FedEx':
-                                    ShippingStatus, LastStep = tr.fedex_track(df.TrackingID[i])
+                        #print "TrackingID: ", pd.isnull(df.TrackingID)
+                        if not pd.isnull(df.TrackingID):
+                                    if tr.get_carrier(df.TrackingID) == 'UPS':
+                                                ShippingStatus, LastStep = tr.ups_track(df.TrackingID, auth)
+                                    if tr.get_carrier(df.TrackingID) == 'FedEx':
+                                                ShippingStatus, LastStep = tr.fedex_track(df.TrackingID, auth)
+
+                        else:
+                                    LastStep = df.DateShipped
+                                    ShippingStatus = "Not available - No Tracking ID!"
+                        #LastStep, ShippingStatus = ""
 
             return LastStep, ShippingStatus
 
@@ -78,17 +90,22 @@ def calc_LastStep(df, Status):
 """
 def calc_waiting(PrioDf, Priority, Status, LastStep):
 
+            #print "Status: ", Status
             now = datetime.now()
 
+            #print now
+
             """ find the row in the Priority dataframe that matches our priority-type """
-            Row = PrioDf.query(Priority)
+            Row = PrioDf.loc[PrioDf['Type - Prio'] == Priority]
+            print("Priority: ", Priority)
 
             """ match  norm-days per step with the current step """
-            for i in range(1,9):
-                        if Status == i:
-                                    NormDays = Row.loc[i]
+            NormDays = Row.iloc[0, Status]
+
+            #print "NormDays: ", NormDays
 
             dt = NormDays - days_between(LastStep, now)
+            #print "dt: ", dt
 
             """
                 I added an overdue-flagm because I thought it might help with sorting.

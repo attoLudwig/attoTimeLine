ab3d818335f26bf014cd294d72ed7459461f50d5
attoLudwig <36920414+attoLudwig@users.noreply.github.com>
2018-05-17 13:12:15 +0200

    Update tracking_f.py

--- tracking_f.py
+++ tracking_f.py
@@ -1,6 +1,10 @@
-from fedex.config import FedexConfig
-from fedex.services.track_service import FedexTrackRequest
-from ClassicUPS import UPSConnection
+#from fedex.config import FedexConfig
+#from fedex.services.track_service import FedexTrackRequest
+#from ClassicUPS import UPSConnection
+from datetime import datetime
+
+
+
 
 def get_carrier(TrackingID):
             if TrackingID[:2] == '1Z':
@@ -18,7 +22,7 @@ def fedex_track(TrackingID, auth):
             '''
                         TO DO!
             '''
-
+            '''
             key = auth[0]
             account_number = auth[1]
             meter_no = auth[2]
@@ -37,54 +41,44 @@ def fedex_track(TrackingID, auth):
 
             """ edit return value! """
             return track.response
+            '''
+            return 'Fedex Tracking not done yet!', datetime.now()
 
 
 def ups_track(TrackingID, auth):
 
-            license_number = auth[0]
-            user_id = auth[1]
-            password = auth[2]
+    return 'UPS Tracking not done yet!', datetime.now()
+            # license_number = auth[4]
+            # user_id = auth[5]
+            # password = auth[6]
+            #
+            # ups = UPSConnection(license_number,
+            #         user_id,
+            #         password,
+            #         debug=True)  # Set to false when ready
+            #
+            # tracking = ups.tracking_info(TrackingID)
+            #
+            # #print tracking.shipment_activities[0]
+            #
+            # last_step = tracking.shipment_activities[0]['Status']['StatusType']['Code']
+            #
+            # date_string = tracking.shipment_activities[0]['Date']
+            # last_date = datetime.strptime(date_string, '%Y%m%d')
+            #
+            # """
+            #             Return description of last step and date of that step
+            # """
+            # if last_step == 'D':
+            #             #signed = tracking.shipment_activities[0]['ActivityLocation']['SignedForByName']
+            #             return 'Delivered.', last_date
+            # elif last_step == 'M':
+            #             return 'Manifest', last_date
+            # elif last_step == 'X':
+            #             return 'Exception', last_date
+            # elif last_step == 'P':
+            #             return 'Pickup', last_date
+            # elif last_step == 'I':
+            #             city = tracking.shipment_activities[0]['ActivityLocation']['Address']['City']
+            #             return 'In transit. Last location: ' + city, last_date
 
-            ups = UPSConnection(license_number,
-                    user_id,
-                    password,
-                    debug=True)  # Set to false when ready
-
-            tracking = ups.tracking_info(TrackingID)
-
-            last_step = tracking.shipment_activities[0]['Status']['StatusType']['Code']
-
-            """
-                        Return description of last step and date of that step
-            """
-            if last_step == 'D':
-                        signed = tracking.shipment_activities[0]['ActivityLocation']['SignedForByName']
-                        return 'Delivered. Signed by: ' + signed, tracking.shipment_activities[0]['Date']
-            elif last_step == 'M':
-                        return 'Manifest', tracking.shipment_activities[0]['Date']
-            elif last_step == 'X':
-                        return 'Exception', tracking.shipment_activities[0]['Date']
-            elif last_step == 'P':
-                        return 'Pickup', tracking.shipment_activities[0]['Date']
-            elif last_step == 'I':
-                        city = tracking.shipment_activities[0]['ActivityLocation']['Address']['City']
-                        return 'In transit. Last location: ' + city, tracking.shipment_activities[0]['Date']
-
-
-'''
-            Testing
-'''
-"""
-#TrackingID = '1Z29340V6670553000'
-
-TrackingID = '735594506654'
-
-shipper = get_carrier(TrackingID)
-
-shipper
-
-test = fedex_track(TrackingID)
-
-test
-test['ActivityLocation']['SignedForByName']
-"""
